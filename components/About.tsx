d20c99762ba8372d109d72c9bc3e49069593b7bf
sorrychoe <cjssoote@gmail.com>
2026-02-10 20:04:55 +0900

    Modified typo

--- components/About.tsx
+++ components/About.tsx
@@ -15,7 +15,7 @@ const About: React.FC = () => {
         <ul className="space-y-4 list-none pl-0 mt-8">
           <li><span className="text-[#b8954b] font-bold">✓ Insight Builder:</span> 한번 주목한 대상은 끝까지 파고들어 전문가 수준의 인사이트를 찾아냅니다.</li>
           <li><span className="text-[#b8954b] font-bold">✓ Trend Interpreter:</span> 트렌드를 소비하기보다, 브랜드에 맞는 방향으로 해석해 전략에 반영합니다.</li>
-          <li><span className="text-[#b8954b] font-bold">✓ Cross Cultural:</span> 트렌드를 소비하기보다, 브랜드에 맞는 방향으로 해석해 전략에 반영합니다.</li>
+          <li><span className="text-[#b8954b] font-bold">✓ Cross Cultural:</span> 북미•아시아•유럽의 문화 경험을 토대로 세상을 바라봅니다.</li>
           <li><span className="text-[#b8954b] font-bold">✓ Concept Creator:</span> 아이디어를 브랜드에 맞는 컨셉과 메시지로 구체화해 실행합니다.</li>
         </ul>
       </div>
